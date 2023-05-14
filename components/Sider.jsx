f62cbb5593aa477721eaf3b70f020fb1edc80088
Alireza Mohseni <mohseni.arz@gmail.com>
2023-05-14 17:16:25 +0330

    install tailwindcss and implement base layout

---
+++ components/Sider.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SiderCom = () => {
+  return (
+    <>
+      <h1 className="mb-5 text-xl font-bold">Welcome 👋</h1>
+      <ul className="list-none grid gap-2 ">
+        <li className="sidebarList">Todos</li>
+        <li className="sidebarList">Add Todo</li>
+        <li className="sidebarList">Profle</li>
+      </ul>
+    </>
+  );
+};
+
+export default SiderCom;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import SiderCom from "@/components/Sider";
+import { Layout } from "antd";
+const { Header, Sider, Footer, Content } = Layout;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Header className="headerStyle">
+        <h1 className="text-3xl">LOGO</h1>
+      </Header>
+      <Layout>
+        <Sider className="siderStyle">
+          <SiderCom />
+        </Sider>
+        <Content className="contentStyle">
+          <Component {...pageProps} />
+        </Content>
+      </Layout>
+      <Footer className="footerStyle">copy right &copy;</Footer>
+    </Layout>
+  );
 }
